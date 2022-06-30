0e0c53e146d72c9cc827c06453e3bd2ce2da1957
Jey Paulraj <jpaulraj@redhat.com>
2022-06-30 14:07:49 +0530

    certificate changes

--- src/main/java/com/httpclient/HttpClient.java
+++ src/main/java/com/httpclient/HttpClient.java
@@ -1,9 +1,7 @@
 package com.httpclient;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -16,19 +14,12 @@ import javax.net.ssl.SSLContext;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
-import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.socket.PlainConnectionSocketFactory;
-import org.apache.http.conn.ssl.DefaultHostnameVerifier;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustAllStrategy;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.ssl.SSLContexts;
 public class HttpClient {
 
@@ -51,126 +42,12 @@ public class HttpClient {
     
 	}
 
-	public  CloseableHttpClient getHttpClientWithCertAuth1() {
-	       
-		
-		    try {
-		        SSLContext  sslContext = SSLContexts
-		                .custom()
-		                .loadTrustMaterial(null, new TrustSelfSignedStrategy())
-		                .build();
-
-		        SSLConnectionSocketFactory sslConnectionFactory = new SSLConnectionSocketFactory(sslContext,
-		                new DefaultHostnameVerifier());
-
-		        Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory> create()
-		                .register("https", sslConnectionFactory)
-		                .register("http", new PlainConnectionSocketFactory())
-		                .build();
-
-		        HttpClientBuilder builder = HttpClientBuilder.create();
-		        builder.setSSLSocketFactory(sslConnectionFactory);
-		        builder.setConnectionManager(new PoolingHttpClientConnectionManager(registry));
-
-		        return builder.build();
-		    } catch (Exception ex) {
-
-		        return null;
-		    }
-
-		}
 	
-
-
-public  CloseableHttpClient getHttpClientWithCertAuth() throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException, KeyManagementException, UnrecoverableKeyException {
-	
-    KeyStore trustStore = KeyStore.getInstance("PKCS12"); //client certificate holder
-
-    FileInputStream instream = new FileInputStream(new File(
-            "client-p12-keystore.p12"));
-    try {
-        trustStore.load(instream, "password".toCharArray());
-    } finally {
-        instream.close();
-    }
-
-    // Trust own CA and all self-signed certs
-    SSLContext sslcontext = SSLContexts.custom()
-            .loadKeyMaterial(trustStore, "password".toCharArray())
-            // .loadTrustMaterial(trustStore, new TrustSelfSignedStrategy()) //if you have a trust store
-            .build();
-    // Allow TLSv1 protocol only
-    SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
-            sslcontext, new String[] { "TLSv1" }, null,
-            SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-    
-    CloseableHttpClient httpclient = HttpClients
-            .custom()
-            .setHostnameVerifier(
-                    SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER) //todo
-            .setSSLSocketFactory(sslsf).build();
-    
-	/*
-	 * try {
-	 * 
-	 * HttpGett httpget = new HttpGet("https://localhost:8443/secure/index");
-	 * 
-	 * System.out.println("executing request" + httpget.getRequestLine());
-	 * 
-	 * CloseableHttpResponse response = httpclient.execute(httpget); try {
-	 * HttpEntity entity = response.getEntity();
-	 * 
-	 * System.out.println("----------------------------------------");
-	 * System.out.println(response.getStatusLine()); if (entity != null) {
-	 * System.out.println("Response content length: " + entity.getContentLength());
-	 * } EntityUtils.consume(entity); } finally { response.close(); } } finally {
-	 * httpclient.close(); }
-	 */
-    
-    return httpclient;
-
-
-}
-
-public CloseableHttpClient getHttpCertAuth() throws IOException{
-	
-	final KeyStore truststore = readStore();
-
-	final SSLContext sslContext;
-	CloseableHttpClient httpClient = null;;
-	
-	  try {
-		sslContext = SSLContexts.custom()
-		      .loadTrustMaterial(truststore, new TrustAllStrategy())
-		     // .loadKeyMaterial(truststore, "keyStorePass".toCharArray(), (aliases, socket) -> "qlikClient")
-		      .loadKeyMaterial(truststore, "keyStorePass".toCharArray())
-		      .build();
-		
-		 httpClient = HttpClients.custom().setSSLContext(sslContext).build();
-	} catch (KeyManagementException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (UnrecoverableKeyException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (NoSuchAlgorithmException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (KeyStoreException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	
-	
-	
-	return  httpClient;
-
-}
 
 public CloseableHttpClient getHttpCertClientAuth() throws IOException, CertificateException{
 	
 	
-	final CloseableHttpClient httpClient;
+	 CloseableHttpClient httpClient = null;;
 	
 	
 		
@@ -178,9 +55,24 @@ public CloseableHttpClient getHttpCertClientAuth() throws IOException, Certifica
 		try {
 			csf = new SSLConnectionSocketFactory(
 					createSslContext(),
-					new String[]{"TLSv1"}, // Allow TLSv1 protocol only
-			        null,
-			        SSLConnectionSocketFactory.getDefaultHostnameVerifier());
+					//new String[]{"TLSv1"}, // Allow TLSv1 protocol only
+			        //null,
+			        //SSLConnectionSocketFactory.getDefaultHostnameVerifier()
+					
+					NoopHostnameVerifier.INSTANCE);
+			
+//			Registry<ConnectionSocketFactory> socketFactoryRegistry = 
+//				      RegistryBuilder.<ConnectionSocketFactory> create()
+//				      .register("https", csf)
+//				      .register("http", new PlainConnectionSocketFactory())
+//				      .build();
+			
+//		    BasicHttpClientConnectionManager connectionManager = 
+//		    	      new BasicHttpClientConnectionManager(socketFactoryRegistry);
+//		    
+//		    	     httpClient = HttpClients.custom().setSSLSocketFactory(csf)
+//		    	      .setConnectionManager(connectionManager).build();
+
 		} catch (KeyManagementException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -202,6 +94,8 @@ public CloseableHttpClient getHttpCertClientAuth() throws IOException, Certifica
 		}
 		
 		httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
+		 
+		
 	 
 	
 	
@@ -211,56 +105,31 @@ public CloseableHttpClient getHttpCertClientAuth() throws IOException, Certifica
 }
 
 public  SSLContext createSslContext() throws KeyStoreException, IOException, NoSuchAlgorithmException, CertificateException, KeyManagementException, UnrecoverableKeyException {
-    // Trusted CA keystore
+    
+	// Trusted CA keystore
 	
 	final String CA_KEYSTORE_TYPE = KeyStore.getDefaultType(); //"JKS";
 	final String CA_KEYSTORE_PATH = System.getProperty("CA_KEYSTORE_PATH", "/cacert.jks") ;
 	final String CA_KEYSTORE_PASS = System.getProperty("CA_KEYSTORE_PASS ", "changeit") ;
 
-	final String CLIENT_KEYSTORE_TYPE = System.getProperty("CLIENT_KEYSTORE_TYPE", "PKCS12") ;
-	final String CLIENT_KEYSTORE_PATH = System.getProperty("CLIENT_KEYSTORE_PATH", "/client.p12") ;
-	final String CLIENT_KEYSTORE_PASS = System.getProperty("CLIENT_KEYSTORE_PASS ", "changeit") ;
+//	final String CLIENT_KEYSTORE_TYPE = System.getProperty("CLIENT_KEYSTORE_TYPE", "PKCS12") ;
+//	final String CLIENT_KEYSTORE_PATH = System.getProperty("CLIENT_KEYSTORE_PATH", "/client.p12") ;
+//	final String CLIENT_KEYSTORE_PASS = System.getProperty("CLIENT_KEYSTORE_PASS ", "changeit") ;
 	
     KeyStore tks = KeyStore.getInstance(CA_KEYSTORE_TYPE);
     tks.load(new FileInputStream(CA_KEYSTORE_PATH), CA_KEYSTORE_PASS.toCharArray());
 
-    // Client keystore
-    KeyStore cks = KeyStore.getInstance(CLIENT_KEYSTORE_TYPE);
-    cks.load(new FileInputStream(CLIENT_KEYSTORE_PATH), CLIENT_KEYSTORE_PASS.toCharArray());
+//    // Client keystore
+//    KeyStore cks = KeyStore.getInstance(CLIENT_KEYSTORE_TYPE);
+//    cks.load(new FileInputStream(CLIENT_KEYSTORE_PATH), CLIENT_KEYSTORE_PASS.toCharArray());
 
     SSLContext sslcontext = SSLContexts.custom()
-            //.loadTrustMaterial(tks, new TrustSelfSignedStrategy()) // use it to customize
-            .loadKeyMaterial(cks, CLIENT_KEYSTORE_PASS.toCharArray()) // load client certificate
+            .loadTrustMaterial(tks, new TrustSelfSignedStrategy()) // use it to customize
+            .loadKeyMaterial(tks, CA_KEYSTORE_PASS.toCharArray()) // load client certificate
             .build();
     return sslcontext;
 }
 
-public KeyStore readStore() throws IOException {
-	  
-	    InputStream keyStoreStream = new FileInputStream("KEYSTORE_PATH" );
-	    KeyStore keyStore = null;
-		try {
-			keyStore = KeyStore.getInstance("PKCS12");
-			 try {
-				keyStore.load(keyStoreStream, "keyStorePass".toCharArray());
-			} catch (NoSuchAlgorithmException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (CertificateException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} catch (KeyStoreException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} // or "JKS"
-	   
-	    return keyStore;
-	  
-	}
 
 
 }
